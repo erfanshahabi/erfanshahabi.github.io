113087ef32ddcca0d1b27707ea836e7230811654
erfan shahabi <erfanshahabi@outlook.com>
2026-08-10 09:02:47 +0330

    update

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+const route = useRoute()
+const siteUrl = 'https://erfanshahabi.com'
+
+useHead({
+  link: [
+    { rel: 'canonical', href: () => `${siteUrl}${route.path}` }
+  ]
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
